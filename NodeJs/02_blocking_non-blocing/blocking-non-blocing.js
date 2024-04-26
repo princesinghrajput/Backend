01df26457661d49333a2de15e21f4542b4d5f0a8
princesinghrajput <psr8084@gmail.com>
2024-04-26 10:24:12 +0530

    updated source codes

--- NodeJs/02_blocking_non-blocing/blocking-non-blocing.js
+++ NodeJs/02_blocking_non-blocing/blocking-non-blocing.js
@@ -1,11 +1,16 @@
-// const fs = require("fs");
-// console.log("1");
-// // ! Sync... Blocking
-// const result = fs.readFileSync("contacts.txt", "utf-8");
-// console.log("2");
-// console.log(result);
+const fs = require("fs");
 
-// console.log("3");
+
+
+console.log("1");
+
+
+
+// ! Sync... Blocking
+const result = fs.readFileSync("contacts.txt", "utf-8");
+console.log(result);
+
+console.log("2");
 
 // // ! Async..... Non-blocking
 
---
+++ python/main.py
@@ -0,0 +1,19 @@
+print("Hello ")
+
+
+#variables....
+
+
+#integer
+a =24
+
+b=a
+
+a=20
+
+name = "Hello"
+
+print(b)
+
+
+
